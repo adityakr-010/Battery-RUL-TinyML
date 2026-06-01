a5e76726b002209a89902a6975cbf7b1be56a5f6
adityakr-010 <adityakr0103454@gmail.com>
2026-06-01 22:36:35 +0530

    Update data_visualisation.m

--- dataset_analysis/data_visualisation.m
+++ dataset_analysis/data_visualisation.m
@@ -1,48 +1,70 @@
-clear; clc; close all;
 
-load('Oxford_Battery_Degradation_Dataset_1.mat');
+clear; clc; close all;
+load('Oxford_Battery_Degradation_Dataset_1.mat'); 
 
 cellData = Cell1;
+cycleNames = fieldnames(cellData);
+numCycles = length(cycleNames);
 
-%  cycle
-cycleName = 'cyc0000';
-data = cellData.(cycleName).C1dc;
 
-t = data.t;
-v = data.v;
-q = data.q;
-T = data.T;
+target_cycle = cycleNames{50}; 
+data_50 = cellData.(target_cycle).C1dc;
 
-t = (t - t(1)) * 24 * 3600;
+t_50 = (data_50.t - data_50.t(1)) * 24 * 3600; 
+v_50 = data_50.v;
+T_50 = data_50.T;
 
-figure('Name','Oxford Battery Cycle Analysis');
 
-subplot(2,2,1)
-plot(t,'LineWidth',1.5)
-title('Elapsed Time')
-xlabel('Sample Index')
-ylabel('Time (s)')
-grid on
+capacity_array = zeros(1, numCycles);
 
-subplot(2,2,2)
-plot(t,v,'LineWidth',1.5)
-title('Voltage Profile')
-xlabel('Time (s)')
+for i = 1:numCycles
+    cyc = cycleNames{i};
+    try
+        q = cellData.(cyc).C1dc.q;
+       
+        capacity_array(i) = abs(q(end) - q(1));
+    catch
+        capacity_array(i) = NaN; 
+    end
+end
+
+fig = figure('Name','Datasetv visualisation');
+
+subplot(2,1,1);
+plot(1:numCycles, capacity_array, '-o', ...
+    'LineWidth', 1.5, ...
+    'MarkerSize', 4);
+
+yline(capacity_array(1)*0.80, ...
+    '--', ...
+    '80% EOL Threshold', ...
+    'LineWidth', 2);
+
+title(' Cell 1 Capacity Degradation');
+xlabel('Cycle Number');
+ylabel('Discharge Capacity (Ah)');
+grid on;
+
+subplot(2,1,2)
+
+yyaxis left
+p1 = plot(t_50, v_50, 'LineWidth', 2);
+p1.Color = [0 0.4470 0.7410];      
+
 ylabel('Voltage (V)')
-grid on
+ax = gca;
+ax.YColor = p1.Color;
 
-subplot(2,2,3)
-plot(t,q,'LineWidth',1.5)
-title('Capacity / Charge')
-xlabel('Time (s)')
-ylabel('Charge (Ah)')
-grid on
+yyaxis right
+p2 = plot(t_50, T_50, 'LineWidth', 2);
+p2.Color = [0.8500 0.3250 0.0980]; 
 
-subplot(2,2,4)
-plot(t,T,'LineWidth',1.5)
-title('Temperature Profile')
-xlabel('Time (s)')
 ylabel('Temperature (°C)')
+ax.YColor = p2.Color;
+
+xlabel('Time (seconds)')
+title(['Internal Dynamics (' target_cycle ')'])
+
 grid on
 
-sgtitle(['Oxford Dataset - ' cycleName])
+sgtitle(' Battery Degradation ');
